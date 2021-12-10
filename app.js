0bf7b0edf7c3dcb27b44b9d9a609c0ae2fae67c4
Turgut Sezer <hackcity@gmail.com>
2021-12-11 00:12:47 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,64 +1,99 @@
 "use strict"
 const dealerHand = []
 const playerHand = []
+const currentDeck = []
 const playerHandEl = document.querySelector(".playerHand .cards")
 const dealerHandEl = document.querySelector(".dealerHand .cards")
 const playerRoundTotalEl = document.querySelector("#player-round-total")
 const dealerRoundTotalEl = document.querySelector("#dealer-round-total")
+const remainingCardsCounterEl = document.querySelector(".remaining-cards > span")
+const playerNameOutputEl = document.querySelector("#player-name-output")
 let playerRoundTotal = 0
 let dealerRoundTotal = 0
-const remainingCardsCounterEl = document.querySelector(".remaining-cards > span")
-let playersTurn = true
+let playersTurn = false
+let bankBalance = 900
 
-// Setting variables >>>
-const playButton = document.querySelector("#play-button")
-let numberOfDecksInput = 4
-let deckUsegeRatioInput = 75
-let deckColorInput = "blue"
 // >>> Setting variables
+const playBtnEl = document.querySelector("#play-btn")
+const playerNameInputEl = document.querySelector("#player-name-input")
+let numberOfDecksInput = 4
+let deckUsegeRatioInput = 100
+let deckColorInput = "blue"
+let playerName = "Player"
+// Setting variables <<<
 
-const deck = {
+/****************
+settings section
+*****************/
+playBtnEl.addEventListener("click", saveSettings)
+function saveSettings() {
+  numberOfDecksInput = document.getElementById("numberOfDecksInput").value
+  const deckUsegeRatioInputElement = document.getElementsByName("prevent-card-counting")
+  deckUsegeRatioInputElement.forEach((item) => {
+    if (item.checked) {
+      deckUsegeRatioInput = item.value
+    }
+  })
+  const deckColorInputElement = document.getElementsByName("deck-color")
+  deckColorInputElement.forEach((item) => {
+    if (item.checked) {
+      deckColorInput = item.value
+    }
+  })
+  playerName = playerNameInputEl.value
+  playerNameOutputEl.textContent = playerName
+
+  document.getElementById("settings-container").classList.toggle("hidden")
+  document.getElementById("betting-container").classList.toggle("hidden")
+
+  betting()
+}
+function betting() {
+  const betBtnEl = document.querySelector("#bet-btn")
+  betBtnEl.addEventListener("click", () => {
+    document.getElementById("betting-container").classList.toggle("hidden")
+    document.getElementById("game-container").classList.toggle("hidden")
+    startRound()
+  })
+}
+
+/****************
+game section
+*****************/
+/* deck copy for debugging
   hearts: ["H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "HT", "HJ", "HQ", "HK", "HA"],
   spades: ["S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "ST", "SJ", "SQ", "SK", "SA"],
   clubs: ["C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "CT", "CJ", "CQ", "CK", "CA"],
   diamonds: ["D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "DT", "DJ", "DQ", "DK", "DA"],
-  full() {
-    return [...this.hearts, ...this.spades, ...this.clubs, ...this.diamonds]
-  },
-  concatDecks(numOfDecks) {
-    let multipleDecks = []
-    for (let i = 0; i < numOfDecks; i++) {
-      multipleDecks = multipleDecks.concat(deck.full())
-    }
-    return multipleDecks
-  },
-  shuffled() {
-    return shuffle(this.concatDecks(numberOfDecksInput))
-  }
+*/
+const deck = {
+  hearts: ["H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "HT", "HJ", "HQ", "HK", "HA"],
+  spades: ["S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "ST", "SJ", "SQ", "SK", "SA"],
+  clubs: ["C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "CT", "CJ", "CQ", "CK", "CA"],
+  diamonds: ["D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "DT", "DJ", "DQ", "DK", "DA"]
 }
+const fullDeck = [...deck.hearts, ...deck.spades, ...deck.clubs, ...deck.diamonds]
 
-function shuffle(arr) {
+function multipleDecks(numOfDeck) {
+  let multipleDecks = []
+  for (let i = 0; i < numOfDeck; i++) {
+    multipleDecks.push(...fullDeck)
+  }
+  console.log("desteler birlestirildi")
+  return multipleDecks
+}
+function shuffle() {
+  const arr = multipleDecks(numberOfDecksInput)
   for (let i = arr.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1))
     ;[arr[i], arr[j]] = [arr[j], arr[i]]
   }
+  console.log("deste karistirildi")
   return arr
-}
-const newDeck = deck.shuffled()
-{
-  // playersTurn 1 kere değiştirilebilir, kagıt dagıtma sırası önemsiz olabilir
-  playerHand.push(popThenCount())
-  playersTurn = false
-  dealerHand.push(popThenCount())
-  playersTurn = true
-  playerHand.push(popThenCount())
-  playersTurn = false
-  dealerHand.push(popThenCount())
-  playersTurn = true
 }
 
 function popThenCount() {
-  let poppedCard = newDeck.pop()
+  let poppedCard = currentDeck.pop()
   let cardFace = poppedCard.slice(-1)
   let cardVal = 0
   // gecerli kartin dosya isminde bu karakterler var mi, regex
@@ -107,17 +142,19 @@ function popThenCount() {
     dealerRoundTotal += cardVal
     dealerRoundTotalEl.textContent = dealerRoundTotal
   }
-  remainingCardsCounterEl.textContent = newDeck.length
+  remainingCardsCounterEl.textContent = currentDeck.length
   return poppedCard
 }
 
-const hitBtnEl = document.querySelector("#hit")
+const hitBtnEl = document.querySelector("#hit-btn")
 hitBtnEl.addEventListener("click", insertPlayerCardToDOM)
-// convert to arrow func if not used more
+// TODO convert to arrow func if not used more
 function insertPlayerCardToDOM() {
+  doubleBtnEl.disabled = true
   playerHand.push(popThenCount())
 }
 function insertDealerCardToDOM() {
+  playersTurn = false
   while (dealerRoundTotal < 18) {
     dealerHand.push(popThenCount())
     if (dealerRoundTotal > 21) {
@@ -126,29 +163,59 @@ function insertDealerCardToDOM() {
   }
 }
 
-const standBtnEl = document.querySelector("#stand")
+const standBtnEl = document.querySelector("#stand-btn")
 standBtnEl.addEventListener("click", stand)
 function stand() {
-  standBtnEl.disabled = true
-  playersTurn = false
+  disableButtons()
+
   insertDealerCardToDOM()
-  console.log("kactim ben")
+  console.log("oyuncu kalkti")
+}
+
+const doubleBtnEl = document.querySelector("#double-btn")
+doubleBtnEl.addEventListener("click", double)
+function double() {
+  console.log("oyuncu iki katini istedi")
+  disableButtons()
+  insertPlayerCardToDOM()
+  insertDealerCardToDOM()
 }
 
 function playerBusted() {
-  // dont forget clear disabled state
-  hitBtnEl.disabled = true
+  disableButtons()
   console.log("oyuncu patladi")
 }
 function dealerBusted() {
   console.log("dealer patladi")
 }
 function playerBJ() {
+  disableButtons()
+  insertDealerCardToDOM()
   console.log("oyuncu elden BlackJack yapti")
 }
 function player21() {
+  disableButtons()
   console.log("oyuncu 21 e ulasti")
 }
 function dealer17() {
   console.log("dealer 17 ye ulasti")
 }
+function disableButtons() {
+  hitBtnEl.disabled = true
+  standBtnEl.disabled = true
+  doubleBtnEl.disabled = true
+}
+
+function startRound() {
+  currentDeck.push(...shuffle())
+  console.log("kagitlar dagitildi")
+  dealerHand.push(popThenCount())
+  dealerHand.push(popThenCount())
+  playersTurn = true
+  playerHand.push(popThenCount())
+  playerHand.push(popThenCount())
+}
+
+/****************
+computing balance
+*****************/
